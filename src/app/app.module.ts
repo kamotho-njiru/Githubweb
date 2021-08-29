85291e3b0604f886e78338e9cf0a63accfecdc67
Moringa School <samuel.kamotho@student.moringaschool.com>
2021-08-29 23:57:13 +0300

    add images to assets folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { GithubService } from './service/github.service';
 
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HoverDirective } from './hover.directive';
