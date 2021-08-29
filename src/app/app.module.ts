5941b9648f98c0a20a921ae1768864f7779e3f1e
Moringa School <samuel.kamotho@student.moringaschool.com>
2021-08-29 21:38:05 +0300

    generate header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HoverDirective } from './hover.directive';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HoverDirective
+    HoverDirective,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
