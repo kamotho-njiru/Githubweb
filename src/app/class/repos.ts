ed2fd1a020484f6abcad200d96cb1f8fee42639f
Moringa School <samuel.kamotho@student.moringaschool.com>
2021-08-29 21:20:27 +0300

    define the blueprint for the repos

--- src/app/class/repos.ts
+++ src/app/class/repos.ts
@@ -1,2 +1,5 @@
 export class Repos {
+    constructor(public name: string, public watches_count:number, public forks_count:number, public stargazers_count:number,
+        public description:string ) {     
+        }
 }
