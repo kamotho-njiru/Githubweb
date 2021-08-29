9e6ca4371416bb2d4d0586a1bda7cbf4285ffc98
Moringa School <samuel.kamotho@student.moringaschool.com>
2021-08-29 21:28:41 +0300

    add hover directive

--- src/app/class/users.ts
+++ src/app/class/users.ts
@@ -1,2 +1,9 @@
 export class Users {
+    constructor(public id:number, public avatar_url:string,
+        public name: string, public html_url:string,
+        public location:string, public public_repos:number,
+        public followers:number, public following:number,
+        public updated_at:Date,){
+            
+        }
 }
