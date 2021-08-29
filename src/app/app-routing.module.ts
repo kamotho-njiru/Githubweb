da9881a2f3ab0708dd94fc9267878c953431ec35
Moringa School <samuel.kamotho@student.moringaschool.com>
2021-08-29 22:59:32 +0300

    nest the child component to parent component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo:'/home'},
+  { path: 'home', component:UsersComponent},
+  { path:'**', component:NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
