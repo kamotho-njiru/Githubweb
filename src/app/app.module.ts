47967f392d044f969bb31d94feb85b3cdfe28584
Moringa School <samuel.kamotho@student.moringaschool.com>
2021-08-29 22:11:41 +0300

    generate 404 pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HoverDirective } from './hover.directive';
 import { HeaderComponent } from './header/header.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HoverDirective,
-    HeaderComponent
+    HeaderComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
